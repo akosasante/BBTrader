b1e8f17ce5ddb83faaa6ad8fbc22f2b5c7548843
Akosua <akosuaasante@gmail.com>
2020-01-07 22:08:50 +0000

    updated to add Graeme

--- client/src/stores/PlayersStore.js
+++ client/src/stores/PlayersStore.js
@@ -1,24 +1,219 @@
 export default {
-    members: [
-        { "_id": { "$oid": "5a007835003ece72d0cac7fe" }, "__v": 0, "name": "Cam and Jatheesh", "username": "cam.macinnis", "email": "the.cam.macinnis@gmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac7ff" }, "__v": 0, "name": "Flex Fox", "username": "flex.fox", "email": "adamlee380@hotmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac800" }, "__v": 0, "name": "Aidan Smith-Edgell", "username": "aidan.smith", "email": "asmithedgell@gmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac801" }, "__v": 0, "name": "Ash Sebastian", "username": "a.sebastian13", "email": "ashley-sebastian@hotmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac802" }, "__v": 0, "name": "Chris Kennedy", "username": "ckennedy361", "email": "ckennedy361@gmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac803" }, "__v": 0, "name": "Garth MacInnis", "username": "garth.macinnis", "email": "garth.macinnis@hotmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac804" }, "__v": 0, "name": "Garth Newton", "username": "garthnewton", "email": "conkeestedoray@gmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac805" }, "__v": 0, "name": "Ian Stadelmann", "username": "stads", "email": "ian.dementors@gmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac806" }, "__v": 0, "name": "Jeffrey Chow", "username": "jeffreytychow", "email": "jeffreytychow@hotmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac807" }, "__v": 0, "name": "Jeffrey Lim", "username": "jlim", "email": "jeffreylim@outlook.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac808" }, "__v": 0, "name": "Jemil Juson", "username": "jemil.jeremiah.juson", "email": "jemiljuson@hotmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac809" }, "__v": 0, "name": "Mike Kaminski", "username": "champion", "email": "mikekam94@gmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac80a" }, "__v": 0, "name": "Nick Penner", "username": "nickpenner", "email": "penner_nick@yahoo.ca" },
-        { "_id": { "$oid": "5a007835003ece72d0cac80b" }, "__v": 0, "name": "Richard Kelly-Ruetz", "username": "rkellyru", "email": "kelly-ruetz@hotmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac80c" }, "__v": 0, "name": "Richard Tillo", "username": "rtillo", "email": "richardtillo@gmail.com" },
-        { "_id": { "$oid": "5a007835003ece72d0cac80e" }, "__v": 0, "name": "Jeremiah Johnson", "username": "jeremiah1993", "email": "jejohnso@uwaterloo.ca" },
-        { "_id": { "$oid": "5a007835003ece72d0cac80f" }, "__v": 0, "name": "Ryan Neeson", "username": "Ryan Neeson", "email": "ryan-neeson@hotmail.com" },
-        {"_id":{"$oid":"5c2ec1c2503edf285cba329d"},"name":"Elie Waitzer","username":"ewaitzer","email":"eliewaitzer@gmail.com","userId":"UF5EXG22H"},
-        {"_id":{"$oid":"5c2ec235503edf285cba329e"},"name":"Luc Hamel","username":"Luc Hamel","email":"luchamel34@hotmail.com","userId":"UF46TTE59"},
-        {"_id":{"$oid":"5c3b7ccb788cfb5cc121cf2d"},"name" : "Benoit Michon", "username" : "Ben", "email" : "tiben0920@gmail.com", "userId" : "UF9T2T9U3"},
-    ]
-}
+      members: [
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac7fe"
+        },
+        "__v": 0,
+        "name": "Cam and Jatheesh",
+        "username": "cam.macinnis",
+        "email": "the.cam.macinnis@gmail.com",
+        "userId": "U0S07R4QG",
+        "password": "$2a$08$hSaoN/RUBV/uwCO79yCzCu0312YH6kpiB9XMQAbvuYePs8umvB.pK"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac7ff"
+        },
+        "__v": 0,
+        "name": "Adam Lee",
+        "username": "flex.fox",
+        "email": "adamlee380@hotmail.com",
+        "userId": "U0S09K0JE",
+        "password": "$2a$08$c909SLh8vhROyFzoxnNXoubitRVJyX3GBMOAtlkVSXIhH8f.00Ag6"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac800"
+        },
+        "__v": 0,
+        "name": "Aidan Smith-Edgell",
+        "username": "aidan.smith",
+        "email": "asmithedgell@gmail.com",
+        "userId": "U0T8MNPQF",
+        "password": "$2a$08$NNJSt/J0MJ0q.ySdS9qiI.b25pvD6fwoLfOGOWiE.v8XvzxREN8Ye"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac801"
+        },
+        "__v": 0,
+        "name": "Ash Sebastian",
+        "username": "a.sebastian13",
+        "email": "ashley-sebastian@hotmail.com",
+        "userId": "U0SED3Y15",
+        "password": "$2a$08$2yxJvxxGPaCZ6KWYKzYLhu.qFVyPl5p6lvtsrF2k1xP9fd7.mj15S"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac802"
+        },
+        "__v": 0,
+        "name": "Chris Kennedy",
+        "username": "ckennedy361",
+        "email": "ckennedy361@gmail.com",
+        "userId": "U414M8ATZ"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac803"
+        },
+        "__v": 0,
+        "name": "Garth MacInnis",
+        "username": "garth.macinnis",
+        "email": "garth.macinnis@hotmail.com",
+        "userId": "U0VN7DU58"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac804"
+        },
+        "__v": 0,
+        "name": "Garth Newton",
+        "username": "garthnewton",
+        "email": "conkeestedoray@gmail.com",
+        "userId": "U0S6HQDGD",
+        "password": "$2a$08$gFyI/um6fEzKAwThY8IKe.YZ0l0EZdhQq0y/5azxe6BEeYEi2w1Ie"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac805"
+        },
+        "__v": 0,
+        "name": "Ian Stadelmann",
+        "username": "stads",
+        "email": "ian.dementors@gmail.com",
+        "userId": "U40DRB0AE",
+        "password": "$2a$08$gtApWrMTOqCTUWXsmtEiFeRvT78L16tUnxKrTKlDpLKLS0APJiURe"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac806"
+        },
+        "__v": 0,
+        "name": "Jeffrey Chow",
+        "username": "jeffreytychow",
+        "email": "jeffreytychow@hotmail.com",
+        "userId": "U0S2LMTSL",
+        "password": "$2a$08$o0T0UJ16iXhLNfM.CTeQoev/p648U7CIE5tTDATrR6YSEMYYMKOli"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac807"
+        },
+        "__v": 0,
+        "name": "Jeffrey Lim",
+        "username": "jlim",
+        "email": "jeffreylim@outlook.com",
+        "userId": "U0T7753L3",
+        "password": "$2a$08$fnqp1H4wP5SW.MtEVOwo4O3kMheD49Cw38trNZe7yaLXobwxu.HMG"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac808"
+        },
+        "__v": 0,
+        "name": "Jemil Juson",
+        "username": "jemil.jeremiah.juson",
+        "email": "jemiljuson@hotmail.com",
+        "userId": "U0SD60VLH",
+        "password": "$2a$08$Rhxi95jblAcWn61HKWbsC.U.9YWD69n/a51bzNcuKiwLl48oIeGki"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac809"
+        },
+        "__v": 0,
+        "name": "Mike Kaminski",
+        "username": "champion",
+        "email": "mikekam94@gmail.com",
+        "userId": "U0SD08TEG",
+        "password": "$2a$08$fRxlxrdR8pX.eL0f5NWThe9MQKShQzEz9YMIW9WD0Ycfwhoau9t.m"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac80a"
+        },
+        "__v": 0,
+        "name": "Nick Penner",
+        "username": "nickpenner",
+        "email": "penner_nick@yahoo.ca",
+        "userId": "U0S09DSBT",
+        "password": "$2a$08$AFyNFXJcV7/3BViGAph26OSUofiKpv6WUc8kYQXEBc7FbpGjY4dUG"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac80b"
+        },
+        "__v": 0,
+        "name": "Richard Kelly-Ruetz",
+        "username": "rkellyru",
+        "email": "kelly-ruetz@hotmail.com",
+        "userId": "U41013ZLL",
+        "password": "$2a$08$kYk9DYBUjKY9fEwkUoUwXuq66uUmmgyzmB2H.9sDEHdEjje4VmGD."
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac80c"
+        },
+        "__v": 0,
+        "name": "Richard Tillo",
+        "username": "rtillo",
+        "email": "richardtillo@gmail.com",
+        "userId": "U41B7UM7H",
+        "password": "$2a$08$91bDzxxiMLKWL5YFJxKoEOEijeq279u55ZT2hd4GxCoha8DKJ2hti"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac80e"
+        },
+        "__v": 0,
+        "name": "Jeremiah Johnson",
+        "username": "jeremiah1993",
+        "email": "jejohnso@uwaterloo.ca",
+        "userId": "U65T580KC",
+        "password": "$2a$08$HSxg1HWAB.iIO1N2LyJDIORgTZDXM4wmxiIUQd.f3xI2rhdJ9kAnG"
+    },
+    {
+        "_id": {
+            "$oid": "5a007835003ece72d0cac80f"
+        },
+        "__v": 0,
+        "name": "Ryan Neeson",
+        "username": "Ryan Neeson",
+        "email": "ryan-neeson@hotmail.com",
+        "userId": "U0S2F7YGJ",
+        "password": "$2a$08$N/C2M84TnoOc5CxQrO9BiemqWy95FQlPY.pzdyKqPIBE.kIoHNiou"
+    },
+    {
+        "_id": {
+            "$oid": "5c2ec235503edf285cba329e"
+        },
+        "name": "Luc Hamel",
+        "username": "Luc Hamel",
+        "email": "luchamel34@hotmail.com",
+        "userId": "UF46TTE59",
+        "password": "$2a$08$0ve4DpiRweflfHDpQ8n7zeLi0i/4cx9SRtR71Ji4f17A72/YrnocO"
+    },
+    {
+        "_id": {
+            "$oid": "5c3b7ccb788cfb5cc121cf2d"
+        },
+        "name": "Benoit Michon",
+        "username": "Ben",
+        "email": "tiben0920@gmail.com",
+        "userId": "UF9T2T9U3",
+        "password": "$2a$08$C4Z22V6xFvfdnINufs6Nx.ewV5BWM39TMNJchLiGAi3jtBUoE4gqO"
+    },
+    {
+        "_id": {
+            "$oid": "5e14e3b5cd1e81dc68d2b141"
+        },
+        "name": "Graeme Kembel",
+        "username": "Graeme",
+        "email": "gkembel1987@gmail.com",
+        "userId": "USD7999BR",
+        "password": ""
+    }
+]
+  }
