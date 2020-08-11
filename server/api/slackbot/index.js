2452a58fdd49353ac75ee298dc6f32b700503591
Akosua Asante <akosuaasante@gmail.com>
2020-08-11 19:35:39 -0400

    FEATURE: New endpoint to only send to sheets by id

--- server/api/slackbot/index.js
+++ server/api/slackbot/index.js
@@ -4,6 +4,7 @@ const slackbotRouter = require('express').Router();
 // const slackbotController = require('./controller');
 const sheetApi = require('../tradesheet/ApiClient');
 const {sendTradeMessage} = require('./new_slackbot');
+const Trade = require('./trade');
 
 slackbotRouter.route('/postTrade')
     .post((req, res) => {
@@ -45,6 +46,38 @@ slackbotRouter.route('/postTrade')
                 console.error(`ERROR FROM NEW SLACKBOT: ${JSON.stringify(slackErr)}`);
                 res.status(500).send({message: 'Something went wrong, please contact admin', error: slackErr});
             });
+    });
+
+slackbotRouter.route('/postSheetsOnly')
+    .post(async (req, res) => {
+        const sheetClient = new sheetApi();
+        await sheetClient.getSheetsClient();
+        const tradeObjs = req.body.trades.map(id => Trade.findById(id).exec());
+        const tradesByRecipient = getTradeDataForSheets(tradeObjs);
+        const recipients = Object.keys(tradesByRecipient);
+        const trades = recipients.map(recip => [
+            recip,
+            tradesByRecipient[recip].players.map(pl => `${pl.player} FROM ${pl.sender}`).join(',\n'),
+            tradesByRecipient[recip].prospects.map(pr => `${pr.prospect} FROM ${pr.sender}`).join(',\n'),
+            tradesByRecipient[recip].picks.map(pi => `${pi.type} - ${pi.round} - ${pi.pick} FROM ${pi.sender}`).join(',\n')
+        ]);
+        const data = trades.reduce((arr, trade, index) => {
+            if (index === 0) {
+                arr.push((new Date()).toISOString().substring(0, 10));
+                arr = arr.concat(trade);
+            } else {
+                arr = arr.concat('', trade);
+            }
+            return arr;
+        }, []);
+        sheetClient.appendToSheet(data, function (sheetsErr, response) {
+            if (sheetsErr) {
+                console.error(sheetsErr);
+                res.status(500).send({message: 'Something went wrong, please contact admin', error: sheetsErr});
+            }
+            console.log(JSON.stringify(response.data, null, 2));
+            res.json({message: 'Trade uploaded to trade inedx sheet only', response: 'success'});
+        });
     });
 
 function getTradeDataForSheets(trades) {
